7cf46ed193f47050f0a2366ebc39f334068a1bbc
salma <abdelhaksalma10@gmail.com>
2026-05-11 03:15:13 +0100

    Implement admin classes

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Course, Lesson, Question, Choice, Submission
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 5
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 5
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text']
+
+# Register your models here
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
